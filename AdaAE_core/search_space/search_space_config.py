930984790876361a3fe648854e5a3274c6c33692
Zhen-Peng-Wu <zhenpeng@csu.edu.cn>
2023-12-18 22:17:27 +0800

    Add files via upload

--- AdaAE_core/search_space/search_space_config.py
+++ AdaAE_core/search_space/search_space_config.py
@@ -7,24 +7,13 @@ class SearchSpace(object):
         self.stack_gnn_architecture = ['augmentation', 'augmentation'] +\
                                       ['attention-aggregation-hidden_dimension', 'activation'] * self.gnn_layers +\
                                       ['pooling']
+
+        self.attention = ['gcn', 'gat', 'graphsage', 'gin']
+        self.aggregation = ['max', 'mean', 'add']
+        self.hidden_dimension = ['32', '64', '128', '256']
         self.space_dict = {
             'augmentation': ['attribute_masking', 'edge_perturbation', 'identical', 'node_dropping', 'subgraph'],
-            'attention-aggregation-hidden_dimension': ['gcn-max-32', 'gcn-mean-32', 'gcn-add-32',
-                                                       'gat-max-32', 'gat-mean-32', 'gat-add-32',
-                                                       'graphsage-max-32', 'graphsage-mean-32', 'graphsage-add-32',
-                                                       'gin-max-32', 'gin-mean-32', 'gin-add-32',
-                                                       'gcn-max-64', 'gcn-mean-64', 'gcn-add-64',
-                                                       'gat-max-64', 'gat-mean-64', 'gat-add-64',
-                                                       'graphsage-max-64', 'graphsage-mean-64', 'graphsage-add-64',
-                                                       'gin-max-64', 'gin-mean-64', 'gin-add-64',
-                                                       'gcn-max-128', 'gcn-mean-128', 'gcn-add-128',
-                                                       'gat-max-128', 'gat-mean-128', 'gat-add-128',
-                                                       'graphsage-max-128', 'graphsage-mean-128', 'graphsage-add-128',
-                                                       'gin-max-128', 'gin-mean-128', 'gin-add-128',
-                                                       'gcn-max-256', 'gcn-mean-256', 'gcn-add-256',
-                                                       'gat-max-256', 'gat-mean-256', 'gat-add-256',
-                                                       'graphsage-max-256', 'graphsage-mean-256', 'graphsage-add-256',
-                                                       'gin-max-256', 'gin-mean-256', 'gin-add-256'],
+            'attention-aggregation-hidden_dimension': ['-'.join([i,j,k]) for k in self.hidden_dimension for j in self.aggregation for i in self.attention],
             'activation': ['elu', 'leaky_relu', 'linear', 'relu', 'relu6', 'sigmoid', 'softplus', 'tanh'],
             'pooling': ['global_max', 'global_mean', 'global_add']
         }
